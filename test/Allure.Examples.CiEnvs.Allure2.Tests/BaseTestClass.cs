85c87ebae1cdd4dccec5dfbb2b8100686244bc82
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-09-08 18:11:41 +0700

    Step 2 - Pass env data to test results

--- test/Allure.Examples.CiEnvs.Allure2.Tests/BaseTestClass.cs
+++ test/Allure.Examples.CiEnvs.Allure2.Tests/BaseTestClass.cs
@@ -1,9 +1,31 @@
+using System;
+using Allure.Net.Commons;
 using Allure.NUnit;
+using NUnit.Framework;
 
 namespace Allure.Examples.CiEnvs.Allure2.Tests;
 
 [AllureNUnit]
 abstract class BaseTestClass
 {
+    [TearDown]
+    public void AddEnvInfo()
+    {
+        var value = Environment.GetEnvironmentVariable($"ALLURE_ENVIRONMENT");
+        if (!string.IsNullOrEmpty(value))
+        {
+            AllureApi.AddTestParameter("env", value);
+            AllureApi.AddParentSuite(value);
+        }
 
+        var type = this.GetType();
+        var @namespace = type.Namespace;
+
+        if (!string.IsNullOrEmpty(@namespace))
+        {
+            AllureApi.AddSuite(@namespace);
+        }
+
+        AllureApi.AddSubSuite(type.Name);
+    }
 }
